2c7448f53b10525085aa952d2c7d517519442c2a
rifatrakib <Rakib.1508@outlook.com>
2022-10-21 07:21:44 +0600

    creating relationships using imperative approach of mapper

--- orm-mapped-class/imperative-mapping.py
+++ orm-mapped-class/imperative-mapping.py
@@ -1,8 +1,17 @@
 from sqlalchemy import Table, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import registry
-
+from sqlalchemy.orm import registry, relationship
 
 mapper_registry = registry()
+Base = mapper_registry.generate_base()
+
+
+class User:
+    pass
+
+
+class Address:
+    pass
+
 
 user_table = Table(
     "user",
@@ -13,9 +22,20 @@ user_table = Table(
     Column("nickname", String(12)),
 )
 
+address = Table(
+    "address",
+    Base.metadata,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", Integer, ForeignKey("user.id")),
+    Column("email_address", String(50)),
+)
 
-class User:
-    pass
+mapper_registry.map_imperatively(
+    User,
+    user_table,
+    properties={
+        "addresses": relationship(Address, backref="user", order_by=address.c.id)
+    },
+)
 
-
-mapper_registry.map_imperatively(User, user_table)
+mapper_registry.map_imperatively(Address, address)
